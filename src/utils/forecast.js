f24379b079f68409d2859de1f22c68deaaab5a6b
Igor Raab <igor.raab@gmail.com>
2019-10-06 22:27:14 +0200

    Add update forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/1617ce227b28bbc83744aab6265f685f/' + longitude + ',' + latitude +'?units=si&lang=sk'
+    const url = 'https://api.darksky.net/forecast/1617ce227b28bbc83744aab6265f685f/' + longitude + ',' + latitude +'?units=si'
 
     request({url, json: true}, (error, { body }) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. And it is ' + body.currently.summary)
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. And it is ' + body.currently.summary + '. The highest temperature today will be ' + body.daily.data[0].temperatureHigh + ' and the lowest temperature is gonna be ' + body.daily.data[0].temperatureLow)
         }
     })
 }
